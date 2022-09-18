7c874e7fd9ccaadba2264b75e8a4a0bf75fb7ac3
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-18 14:13:44 -0300

    retrieve employee list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './interfaces/employee';
+import { EmployeeService } from './services/employee/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'employee-manager-app';
+export class AppComponent implements OnInit {
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.message);
+      }
+    );
+  }
 }
--- src/app/services/employee/employee.service.ts
+++ src/app/services/employee/employee.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from 'src/app/interfaces/employee';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  private readonly apiServerUrl = '';
+  private readonly apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
