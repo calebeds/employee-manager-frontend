6bbcfeab99f3110efb2dc4aa6b68a4901d074d13
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-09-19 13:11:26 -0300

    implement add and edit employee modals to save data on the backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './interfaces/employee';
 import { EmployeeService } from './services/employee/employee.service';
 
@@ -10,6 +11,7 @@ import { EmployeeService } from './services/employee/employee.service';
 })
 export class AppComponent implements OnInit {
   public employees: Employee[] = [];
+  public editEmployee: Employee = {} as Employee;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -28,7 +30,8 @@ export class AppComponent implements OnInit {
     );
   }
 
-  public onOpenModal(employee: Employee, mode: string): void {
+  public onOpenModal(mode: string, employee: Employee = {} as Employee): void {
+    this.editEmployee = employee;
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
@@ -37,5 +40,33 @@ export class AppComponent implements OnInit {
     button.setAttribute('data-target', `#${mode}EmployeeModal`);
     container?.appendChild(button);
     button.click();
+    button.remove();
+  }
+
+  public onAddEmployee(addForm: NgForm): void {
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        document.getElementById('add-employee-form')?.click(); //Click to close the modal
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        console.error(error.message);
+      }
+    );
   }
 }
